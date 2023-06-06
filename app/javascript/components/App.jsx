34f78c0c966ea6223a1584e89c53a90d740a491a
Mael Harnois <maelh@MBP-MAEL.local>
2023-06-06 22:56:45 +0800

    clean display: authors name only

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,23 +4,12 @@ import ChatContainer from "./ChatContainer";
 
 function App({ cable }) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full gap-0.5 bg-black bg-opacity-30 backdrop-blur">
-      <div className="flex items-center justify-between gap-2 font-thin text-white sm:w-1/2 lg:min-w-1/3">
-        <h1 className="text-6xl">Ask books</h1>
+    <div className="px-6 flex flex-col items-center justify-center w-full h-full gap-0.5 bg-black bg-opacity-30 backdrop-blur">
+      <div className="flex items-center justify-between w-full gap-2 font-thin text-white sm:w-1/2 lg:min-w-1/3">
+        <h1 className="text-3xl md:text-6xl">Ask books</h1>
         <div className="flex items-end justify-center h-full">
-          <div className="grid grid-cols-2 gap-0.5">
-            <div className="px-2 py-1 text-xs text-center bg-black rounded-tl">
-              Currently
-            </div>
-            <div className="px-2 py-1 text-xs text-center bg-black rounded-tr">
-              Upcoming
-            </div>
-            <div className="px-2 text-lg text-center bg-black rounded-bl">
-              🥸 Marx
-            </div>
-            <div className="px-2 text-lg text-center bg-black rounded-br">
-              ⏳ 👀
-            </div>
+          <div className="px-2 text-lg text-center bg-black rounded">
+            🥸 Karl Marx
           </div>
         </div>
       </div>
