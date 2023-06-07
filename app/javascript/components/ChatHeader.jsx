f13ea5430a85a3b118429d581e75764e13cdd238
Mael Harnois <maelh@MBP-MAEL.local>
2023-06-07 11:22:31 +0800

    update with ChatGPT & fix Gumroad link

--- app/javascript/components/ChatHeader.jsx
+++ app/javascript/components/ChatHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ChatHeader() {
   return (
     <div className="flex items-center justify-between w-full gap-2 font-thin text-white sm:w-1/2 lg:min-w-1/3">
-      <h1 className="text-3xl md:text-6xl">Ask Karl Marx 🥸</h1>
+      <h1 className="text-2xl md:text-4xl">ChatGPT & Karl Marx 🥸</h1>
     </div>
   );
 }
--- app/javascript/components/SupportOnGumroad.jsx
+++ app/javascript/components/SupportOnGumroad.jsx
@@ -4,8 +4,9 @@ function SupportOnGumroad({ lastMessage, role, content }) {
   return (
     <div className="">
       <a
-        href="https://maelus.gumroad.com/askmarx"
+        href="https://maelus.gumroad.com/l/askmarx"
         className="p-1 px-2 font-thin text-center text-white bg-black rounded-b bg-opacity-30 hover:bg-black hover:scale-110"
+        target="_blank"
       >
         Support the project on Gumroad 🫶
       </a>
